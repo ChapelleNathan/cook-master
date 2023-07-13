61326fc67d131b64ea10ab7523ab2f5ccb81e6cb
Halphane <ncdev@outlook.fr>
2023-07-13 16:31:57 +0200

    init spa

---
+++ api/public/javascripts/utils/_createMarkup.js
@@ -0,0 +1,11 @@
+export default function createMarkup(markupname, text, parent, attributes = []) {
+    const markup = document.createElement(markupname);
+    markup.textContent = text;
+    parent.appendChild(markup);
+    for (const attribute of attributes) {
+        for (let key in attribute) {
+            markup.setAttribute(key, attribute[key]);
+        }
+    }
+    return markup;
+}
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { resolve } = require('path')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/recipes', function(req, res) {
+  res.sendFile(resolve('public','html', 'index.html'));
 });
 
 
