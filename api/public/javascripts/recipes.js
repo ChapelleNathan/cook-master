1c6e51c26ecfb164165651f4e0a943b724650c58
Mexane <mexane.bonaventure@yahoo.com>
2023-07-17 11:47:08 +0200

    Début recipe

--- api/public/javascripts/recipes.js
+++ api/public/javascripts/recipes.js
@@ -1,5 +1,33 @@
 import createMarkup from "./utils/_createMarkup.js";
 
 export function recipes() {
-    //TODO Fetch all recipes
+    fetch("../../db/db.json")
+      .then(function(response) {
+        if (response.status !== 200) { // si ça c'est mal passé
+        throw new Error("Le serveur n'a pas répondu correctement");
+        } else {
+            return response.json(); // renvoie une promesse
+        }
+      })
+      .then(function(data) {
+        const recipes = data;
+        console.log(recipes);
+        recipes.forEach(function(recipe){
+            render(recipe);
+        });
+      })
+}
+
+
+
+export function render(recette) {
+    const recipeList = createMarkup("section","",document.body); //temporaire
+    const recipeBox= createMarkup("section","",recipeList);
+    createMarkup("h3",recette.title,recipeBox);
+    console.log(recette.title,recipeBox);
+    createMarkup("ul","",recipeBox);
+    recette.ingredients.forEach(function(ingredient){
+        createMarkup("li", ingredient.quantity + " " + ingredient.name + " " + ingredient.unit.alias);
+        console.log(ingredient.quantity + " " + ingredient.name + " " + ingredient.unit.alias);
+    });
 }
