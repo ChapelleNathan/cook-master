2354f76bd1f351902fe4cf773ad0debe808d2db3
Halphane <ncdev@outlook.fr>
2023-07-17 11:43:29 +0200

    can view all recipes

---
+++ api/controller/recipeController.js
@@ -0,0 +1,5 @@
+const { recipes } = require('../db/db.json');
+
+exports.getAllRecipes = ((req, res) => {
+    res.status(200).json(recipes);
+})
--- api/public/javascripts/recipes.js
+++ api/public/javascripts/recipes.js
@@ -1,5 +1,10 @@
 import createMarkup from "./utils/_createMarkup.js";
 
-export function recipes() {
-    //TODO Fetch all recipes
+export async function recipes() {
+    const section = document.getElementById('recipes');
+    let recipes = await((await fetch('/api/recipes')).json());
+    recipes.forEach(recipe => {
+        const article = createMarkup('article','', section)
+        createMarkup('a',`${recipe.title}`, article, [{href: `/recipe?id=${recipe.id}`}, {class: 'fs-4'}]);
+    });
 }
