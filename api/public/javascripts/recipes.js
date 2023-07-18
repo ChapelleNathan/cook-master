8294e65215050c119f7d2cff8eb7f7149d1fdde9
Halphane <ncdev@outlook.fr>
2023-07-18 11:29:04 +0200

    read recipe work with all pages

--- api/public/javascripts/recipes.js
+++ api/public/javascripts/recipes.js
@@ -2,24 +2,25 @@ import createMarkup from "./utils/_createMarkup.js";
 
 export async function recipes() {
     const section = document.getElementById('recipes');
-    let recipesJSON = await((await fetch('/api/recipes')).json());
+    let recipesJSON = await ((await fetch('/api/recipes')).json());
     recipesJSON.forEach(country => {
         let recipes = country.recipes;
         recipes.forEach(recipe => {
-            const article = createMarkup('article','', section)
-            createMarkup('a',`${recipe.title}`, article, [{href: `/recipe?id=${recipe.id}`}, {class: 'fs-4'}]);
+            const article = createMarkup('article', '', section)
+            createMarkup('a', `${recipe.title}`, article, [{ href: `/recipe?id=${recipe.id}` }, { class: 'fs-4' }]);
         })
     })
 }
 
 
 export async function recipe(id) {
-    const recipeInfo= createMarkup("section","",document.body);
-    let recipe = await((await fetch(`/api/recipe?id=${id}`)).json());
-    console.log(recipe);
+    const recipeInfo = document.getElementById('recipeDetail');
+    const recipe = await ((await fetch(`/api/recipe?id=${id}`)).json());
+    const units = await ((await fetch('/api/units')).json());
     createMarkup("h3", recipe.title, recipeInfo);
-    createMarkup("ul", "", recipeInfo);
-    recipe.ingredients.forEach(function(ingredient){
-        createMarkup("li", ingredient.quantity + " " + ingredient.name + " " + ingredient.unit.alias, recipeInfo);
+    const list = createMarkup("ul", "", recipeInfo);
+    recipe.ingredients.forEach(function (ingredient) {
+        const unit = units.find(unit => unit.name == ingredient.unit);
+        createMarkup('li', `${ingredient.quantity} ${unit.alias} de ${ingredient.name}`, list)
     });
 }
