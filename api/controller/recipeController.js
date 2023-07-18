c023fdd8e40af94555601301fb6d73afb9855a11
Debart Nathan <denathan@laposte.net>
2023-07-18 11:03:41 +0200

    api : get recipes : added query filtering

--- api/controller/recipeController.js
+++ api/controller/recipeController.js
@@ -1,5 +1,41 @@
 const { recipes } = require('../db/db.json');
 
-exports.getAllRecipes = ((req, res) => {
-    res.status(200).json(recipes);
+exports.getRecipes = ((req, res) => {
+    const filteredRecipes = recipes
+    if (req.query.gastronomy != "") {
+        filteredRecipes.recipes = filterGastronomy(filteredRecipes.recipes,req.query.gastronomy)
+    }
+    if (req.query.ingredient != "") {
+        filteredRecipes.recipes =filterIngredient(filteredRecipes.recipes,req.query.ingredient)
+    }
+    res.status(200).json(filteredRecipes);
 })
+
+/**
+ * filter the unwanted gastronomy
+ * @param {Object} data recipes object grouped by gastronomy
+ * @param {string} filter name of the gastronomy to filter
+ * @returns the recipes of the wanted gastronomy while preserving the structure of data
+ */
+function filterGastronomy(data,filter){
+    return data.filter(gastronomy => {
+        return gastronomy.name == filter
+    });
+}
+
+/**
+ * filter recipes with unwanted ingredient
+ * @param {Object} data recipes object grouped by gastronomy
+ * @param {string} filter name of the ingredient to filter
+ * @returns the recipes with the wanted ingredient while preserving the structure of data
+ */
+function filterIngredient(data,filter){
+    return data.recipes.map(gastronomy=>{
+        return gastronomy.recipes.filter(recipe=>{
+            
+            return 0 != recipe.ingredients.filter(ingredient=>{
+                return ingredient.name == filter;
+            }).length
+        })
+    });
+}
--- api/routes/recipes.routes.js
+++ api/routes/recipes.routes.js
@@ -2,6 +2,6 @@ const { getAllRecipes } = require('../controller/recipeController');
 
 const router = require('express').Router();
 
-router.get('/api/recipes', getAllRecipes)
+router.get('/api/recipes', getRecipes)
 
 module.exports = router;
