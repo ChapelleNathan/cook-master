47cf994d00c029f61cc5cbc8945c2ffa34dda71f
Halphane <ncdev@outlook.fr>
2023-07-20 14:37:42 +0200

    use spa for recipes

--- public/javascripts/recipes.js
+++ public/javascripts/recipes.js
@@ -59,7 +59,7 @@ export async function recipes() {
             }
             recipes.forEach(recipe => {
                 const article = createMarkup('article', '', section, [{class: 'd-flex justify-content-between mb-2 ms-4'}])
-                createMarkup('a', `${recipe.title}`, article, [{ href: `/recipe?id=${recipe.id}` }, { class: 'fs-4' }]);
+                createMarkup('a', `${recipe.title}`, article, [{ href: `/recipe?id=${recipe.id}` }, { class: 'fs-4' }, {onclick:'route(event)'}]);
                 const suppBtn = createMarkup('button','Supprimer', article, [{class: 'btn btn-danger btn-sm'}]);
                 suppBtn.addEventListener('click', async (event) => {
                   event.preventDefault();
